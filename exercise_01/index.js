79845509975448eaae3b43a75613a11a69e841b8
panambunangiver <panambunangiver01@gmail.com>
2022-03-03 20:19:30 +0700

    menambahkan comment di index.js

--- exercise_01/index.js
+++ exercise_01/index.js
@@ -1,15 +1,33 @@
 //senin
-
 console.log("Baris Pertama");
-
-//selasa
 console.log("Baris Kedua");
 console.log("Baris Ketiga");
-
-//Rabu
 console.log("Baris Keempat");
-console.log("Baris Kelima");
 
-//Kamis
+//selasa
+console.log("Baris Kelima");
 console.log("Baris Keenam");
 console.log("Baris Ketujuh");
+console.log("Baris Kedelapan");
+
+//Rabu
+console.log("Baris kesembilan");
+console.log("Baris Kesepuluh");
+console.log("Baris Kesebelas");
+console.log("Baris ke-duabelas");
+
+//Kamis
+console.log("Baris Ke-tigabelas");
+console.log("Baris Ke-empatbelas");
+console.log("Baris Ke-limabelas");
+console.log("Baris ke-enambelas");
+
+//Jumat
+console.log("Baris Ke-tujuhbelas");
+console.log("Baris Ke-delapanbelas");
+
+//Sabtu
+console.log("Baris ke-sembilanbelas");
+
+//Minggu
+console.log("Baris Ke-duapuluh");
