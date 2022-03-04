d03d40178b65ee90aa070c6c2c569049d1d7e261
michaeldanieljefferson <s21910405@student.unklab.ac.id>
2022-03-04 15:02:25 +0800

    menambahkan nama di body exercise_06

--- components/Body.js
+++ components/Body.js
@@ -10,8 +10,8 @@ export default class Body extends Component {
             <View style={styles.container}>
                 <Text>1. PANAMBUNAN GIVER G.A (Ketua)</Text>
                 <Text>2. HALID MUHAMMAD (Anggota)</Text>
-                <Text>3. (Anggota)</Text>
-                <Text>4. (Anggota)</Text>
+                <Text>3. NOVRANDO NATANAEL HIZKIA (Anggota)</Text>
+                <Text>4. MICHAEL DANIEL JEFFERSON (Anggota)</Text>
                 
             </View>
         );
