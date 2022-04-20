02370d9246101885b454bc182668c2244df2ea2c
panambunangiver <panambunangiver01@gmail.com>
2022-04-20 08:43:35 +0700

    menambahkan exercise_09

---
+++ src/components/atoms/Team/index.js
@@ -0,0 +1,8 @@
+import { View } from 'react-native'
+import React from 'react'
+
+const Team = ({width, height}) => {
+  return <View style={{width: width, height: height}} />
+}
+
+export default Team
---
+++ src/components/atoms/index.js
@@ -0,0 +1,6 @@
+import Button from "./Button";
+import TextInput from "./Textinput";
+import Team from "./Team";
+import Label from "./Label";
+
+export {Button, TextInput, Team, Label};
